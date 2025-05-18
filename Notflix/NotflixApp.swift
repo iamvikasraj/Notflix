2147346564fef0f535eaa8a31895f32ce6c1a037
Vikas Raj Yadav <vikasyadav.vision111@gmail.com>
2025-05-18 13:07:41 +0530

    Initial Commit

---
+++ Notflix/NotflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotflixApp.swift
+//  Notflix
+//
+//  Created by Vikas Raj Yadav on 18/05/25.
+//
+
+import SwiftUI
+
+@main
+struct NotflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotflixTests/NotflixTests.swift
@@ -0,0 +1,16 @@
+//
+//  NotflixTests.swift
+//  NotflixTests
+//
+//  Created by Vikas Raj Yadav on 18/05/25.
+//
+
+import Testing
+
+struct NotflixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
